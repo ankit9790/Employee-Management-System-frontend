dd849e907e7208356ecd0a94cb18cc213b01f0a3
ankit <ankity9790@gmail.com>
2025-09-28 12:30:57 +0530

    remove .env file

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   // baseURL: "https://employee-management-system-backend-traf.onrender.com/api",
-   baseURL:"http://localhost:3000/api",
+   baseURL: "https://employee-management-system-backend-12w2.onrender.com/api",
+   // baseURL:"http://localhost:3000/api",
 });
 
 export default API;
